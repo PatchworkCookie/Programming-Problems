0f2fcbd984f772d675c4de0fb227b53dd7f72b89
Evan <thegeekforhire@gmail.com>
2019-05-15 17:11:48 -0500

    Fixed typo in comment

--- test_listAlternator.py
+++ test_listAlternator.py
@@ -1,5 +1,5 @@
 '''
-Tests for listSums.py
+Tests for listAlternator.py
 '''
 import pytest, listAlternator
 
@@ -8,7 +8,6 @@ import pytest, listAlternator
 (['s','m','t','i','g','s','o','k'],['o','e','h','n',' ','p','o','y'],['s','o','m','e','t','h','i','n','g',' ','s','p','o','o','k','y']),
 (['t','e','s','t','i','n','g'],['1','2','3'],['t','1','e','2','s','3','t','i','n','g']),
 (['4','5','6'],['t','h','i','n','g','s'],['4','t','5','h','6','i','n','g','s'])])
-
 def test_alternateLists(input1, input2, exOutput):
 	result = listAlternator.alternateLists(input1, input2)
 	assert result == exOutput
